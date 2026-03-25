e8a5f698d3d7769394b944a95aaed392bb537aed
OscarLopez890 <oscarcastillolopez890@gmail.com>
2026-03-24 20:11:10 -0600

    feat: Add the header structure with out styles
    Add the header with navigate and logo

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import { Navigation } from "./Navigation"
+
+export function Header(){
+    return (
+        <header>
+            <img src="src/images/logo.png" alt="Logo"></img>
+            <Navigation />
+        </header>
+    )
+}
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,13 @@
+export function Navigation(){
+    return (
+        <nav>
+            <ul>
+                <li><a href="#home">Inicio</a></li>
+                <li><a href="#experience">Experiencia</a></li>
+                <li><a href="#projects">Proyectos</a></li>
+                <li><a href="#contact">Contacto</a></li>
+                <li><a href="#about">Acerca de mi</a></li>
+            </ul>
+        </nav>
+    )
+}
