240ded8f7846e19223c86f6ab2e8e499abf274e9
OscarLopez890 <oscarcastillolopez890@gmail.com>
2026-03-28 19:53:24 -0600

    feat: Create structure of experience
    Create section of experience and cards with details of experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,30 @@
+import { ExperienceCard } from "./ExperienceCard"
+
 export function Experience() {
     return (
-        <div>
-            <h1>Experience</h1>
-        </div>
+        <section>
+            <h1>Experiencia</h1>
+            <ExperienceCard
+                title="Desarrollador Web FullStack | Freelance"
+                description="Implementé un sistema de gestión de inventarios para un taller de fabricación de pantalones, 
+                mejorando la eficiencia en el seguimiento de productos e inventarios para la fabricación y venta."
+                tecnologies={["CSS", "React", "Spring Boot", "PostgreSQL"]}
+                image="src/images/Programador.png"
+            />
+            <ExperienceCard
+                title="Desarrollador Web FullStack | CIATEQ"
+                description="Lideré el desarrollo de un sistema institucional orientado a centralizar 
+                    procesos administrativos, normativos y de mejora continua."
+                tecnologies={["HTML", "CSS", "JavaScript", "PHP", "MariaDB", "Bootstrap"]}
+                image="src/images/ciateq.png"
+            />
+            <ExperienceCard
+                title="Desarrollador Web FullStack | ITESA"
+                description="Implementé mejoras y ampliaciones de un sistema web para la gestión y 
+                evaluación de artículos científicos con la implementación de un modelo de IA."
+                tecnologies={["CSS", "React", "Node.js", "MongoDB Atlas"]}
+                image="src/images/itesa.png"
+            />
+        </section>
     )
 }
--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -1,7 +1,16 @@
-export function ExperienceCard(){
+export function ExperienceCard({title, description, tecnologies, image}) {
     return (
         <div>
-            <h1>ExperienceCard</h1>
+            <img src={image} alt={title}/>
+            <div>
+                <h2>{title}</h2>
+                <p>{description}</p>
+            </div>
+            <li>
+                {tecnologies.map((tecnology, index) => (
+                    <ul key={index}>{tecnology}</ul>
+                ))}
+            </li>
         </div>
     )
 }
