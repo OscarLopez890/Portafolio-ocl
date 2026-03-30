9eaa449d5a92a25b4521f3a20e896d278edefa61
OscarLopez890 <oscarcastillolopez890@gmail.com>
2026-03-29 22:03:55 -0600

    refactor: Create folders to organizate components
    Create some folders for Contact, Header, Navigation and Presentation, and fix errors aboute move files to the new folder

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Navigation } from "./Navigation"
+import { Navigation } from "../Navigation/Navigation"
 
 export function Header(){
     return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
-import { Presentation } from "../components/Presentation"
+import { Presentation } from "../components/Presentation/Presentation"
 import { Experience } from "../components/Experience/Experience"
 import { Projects } from "../components/Project/Projects"
 import { Tecnologies } from "../components/Tecnologies/Tecnologies"
-import { Contact } from "../components/Contact"
+import { Contact } from "../components/Contact/Contact"
 
 export default function Home() {
     return (
