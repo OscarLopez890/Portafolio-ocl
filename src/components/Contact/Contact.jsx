7961a68e32ee33c0e0331ac8d8dd4fb7a9dfc4cb
OscarLopez890 <oscarcastillolopez890@gmail.com>
2026-03-31 21:22:30 -0600

    style: Implemented styles to contact section
    Implemented styles and create a file named Contact.module.css to add styles

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,18 @@
+import styles from './Contact.module.css';
+
 export function Contact() {
     return (
-        <section>
+        <section className={styles.contactContainer}>
             <h1>Contacto</h1>
             <p>Si deseas contactarme:</p>
-            <a href="mailto:tu-email@example.com">
-                <img src="src/images/email.png" alt="Email Icon" />
-            </a>
-            <a href="https://www.linkedin.com/in/tu-perfil-linkedin" target="_blank" rel="noopener noreferrer">
-                <img src="src/images/linkedin.png" alt="LinkedIn Icon" />
-            </a>
+            <div className={styles.contactIcons}>
+                <a href="mailto:tu-email@example.com">
+                    <img src="src/images/Gmail.png" alt="Email Icon" />
+                </a>
+                <a href="https://www.linkedin.com/in/tu-perfil-linkedin" target="_blank" rel="noopener noreferrer">
+                    <img src="src/images/Linkedin.png" alt="LinkedIn Icon" />
+                </a>
+            </div>
         </section>
     )
 }
