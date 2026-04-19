4425f8c126ac6d86c8e78ba70671ff073000b99a
OscarLopez890 <oscarcastillolopez890@gmail.com>
2026-04-19 12:42:23 -0600

    style: Created some styles to the header
    Now, header is static in the top of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { Header } from "./components/Header/Header.jsx"
+import { Header }  from "./components/Header/Header.jsx"
 import Home from "./pages/Home.jsx"
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Header/>
       <Home/>
-    </>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,16 @@ import { Experience } from "../components/Experience/Experience"
 import { Projects } from "../components/Project/Projects"
 import { Tecnologies } from "../components/Tecnologies/Tecnologies"
 import { Contact } from "../components/Contact/Contact"
+import styles from "./Home.module.css"
 
 export default function Home() {
     return (
-        <>
+        <div className={styles.homeContainer}>
             <Presentation/>
             <Experience/> 
             <Projects/>
             <Tecnologies/>
             <Contact/>
-        </>
+        </div>
     )
 }
